a5590610a9242bbb5e08fa0a1a283dc1563afbed
Joel <joelbmolina@gmail.com>
2017-04-25 22:05:43 -0700

    implemented categories reducers into app

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
+import categories from './categories'
 import todos from './todos'
 import visibilityFilter from './visibilityFilter'
 
 const todoApp = combineReducers({
   todos,
+  categories,
   visibilityFilter
 })
 
