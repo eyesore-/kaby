19c57985e12d5640987ec1b8e49321c4181d863c
Joel <joelbmolina@gmail.com>
2017-04-25 22:06:20 -0700

    added action to add a new category to categories list

--- src/client/containers/AddTodo.js
+++ src/client/containers/AddTodo.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { addTodo } from '../actions/index'
+import { addTodo, addCategory } from '../actions/index'
 import { extractCategory, extractText } from '../utils/helpers'
 
 let AddTodo = ({ dispatch }) => {
@@ -14,6 +14,7 @@ let AddTodo = ({ dispatch }) => {
       newTodo.category = extractCategory(input.value)
       newTodo.text = extractText(input.value)
       dispatch(addTodo(newTodo))
+      if (newTodo.category) dispatch(addCategory(newTodo.category))
       input.value = ''
     }}>
       <input ref={node => {
